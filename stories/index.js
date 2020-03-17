7727f9538b165a88fd04d4606ba277aba7008ad1
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-03-16 21:52:02 -0700

    finished  DayListItem components

--- stories/index.js
+++ stories/index.js
@@ -5,6 +5,7 @@ import { action } from "@storybook/addon-actions";
 
 import "index.scss";
 
+import DayListItem from "components/DayListItem";
 import Button from "components/Button";
 
 storiesOf("Button", module)
@@ -21,4 +22,17 @@ storiesOf("Button", module)
     <Button disabled onClick={action("button-clicked")}>
       Disabled
     </Button>
+  ));
+
+
+
+  storiesOf("DayListItem", module) 
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => <DayListItem name="Monday" spots={5} />)
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
+  .add("Full", () => <DayListItem name="Monday" spots={0} />)
+  .add("Clickable", () => (
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> 
   ));
