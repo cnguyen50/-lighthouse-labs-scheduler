04e7f0b229028a8012533e4ce1ad042c2af50b32
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-03-17 00:12:59 -0700

    daylist component and imported daylistitems

--- stories/index.js
+++ stories/index.js
@@ -5,6 +5,7 @@ import { action } from "@storybook/addon-actions";
 
 import "index.scss";
 
+import DayList from "components/DayList";
 import DayListItem from "components/DayListItem";
 import Button from "components/Button";
 
@@ -36,3 +37,33 @@ storiesOf("Button", module)
   .add("Clickable", () => (
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> 
   ));
+
+
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
+  
+  storiesOf("DayList", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+    })
+    .add("Monday", () => (
+      <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+    ))
+    .add("Tuesday", () => (
+      <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+    ));
