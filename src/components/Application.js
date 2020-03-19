66736f158e9bb213fed8aca09251179aef5941f6
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-03-18 20:23:54 -0700

    combined state, useEffect with axios GET and renders once

--- src/components/Application.js
+++ src/components/Application.js
@@ -50,18 +50,21 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState(prev => ({ ...prev, day }));
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
-    axios.get("/api/days")
-    .then((res) => {
-      setDays(res.data)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  })
+    axios.get('/api/days').then(response => setDays(response.data));
+  }, [])
   
   return (
     <main className="layout">
@@ -73,8 +76,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList
-          days={days}
-          day={day}
+          days={state.days}
+          day={state.day}
           setDay={setDay}
         />
       </nav>
